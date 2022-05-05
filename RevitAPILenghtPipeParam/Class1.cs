122f04d4f0c435ec023b75bd0697958833e9c360
Skitt2 <provaid@yandex.ru>
2022-05-05 21:43:47 +0300

    Add project files.

---
+++ RevitAPILenghtPipeParam/Class1.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILenghtPipeParam
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Application application = uiapp.Application;
+
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new PipeFilter(), "Выберите трубы");
+
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Pipe oPipe = doc.GetElement(selectedElement) as Pipe;
+
+                double lengthParam = oPipe.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+
+                double extraLength = lengthParam * 1.1 / 1000;
+
+                var categorySet = new CategorySet();
+                categorySet.Insert(Category.GetCategory(doc, BuiltInCategory.OST_PipeCurves));
+
+                using (Transaction ts = new Transaction(doc, "Set parameter"))
+                {
+                    ts.Start();
+                    Parameter TestParameter = oPipe.LookupParameter("Длина с коэф 1.1");
+                    TestParameter.Set(extraLength);
+
+                    ts.Commit();
+                }
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
